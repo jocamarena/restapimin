7bfa6380cd39b52ab74136afd1d0a2149ef0945a
Jose Camarena <jose.j.camarena@gmail.com>
2024-01-01 10:03:27 -0800

    initialized project

---
+++ src/main/java/com/example/restapimin/RestapiminApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapimin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapimin/RestapiminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapimin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
