8ae86793d71effa47be0ae1b87f72c3d00c827a5
Jose Camarena <jose.j.camarena@gmail.com>
2024-01-01 10:26:48 -0800

    saved greeting restcontroller

---
+++ src/main/java/com/example/restapimin/api/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.restapimin.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greeting")
+public class Greeting {
+    @GetMapping
+    public ResponseEntity<String> getGreeting() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
